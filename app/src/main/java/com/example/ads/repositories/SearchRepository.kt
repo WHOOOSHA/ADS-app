dcab5ba71baaf1e556e242216436500a95a363aa
WHOOOSHA <thisfansyou@gmail.com>
2025-03-18 01:04:59 +0700

    Репозиторий переписан асинхронно

--- app/src/main/java/com/example/ads/repositories/SearchRepository.kt
+++ app/src/main/java/com/example/ads/repositories/SearchRepository.kt
@@ -1,9 +1,11 @@
 package com.example.ads.repositories
 
-
-import com.example.ads.data.Group
 import com.example.ads.data.Ad
+import com.example.ads.data.Group
 import com.example.ads.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class SearchRepository {
     private val categories = listOf("Авто", "Недвижимость", "Электроника", "Услуги", "Работа", "Одежда", "Животные", "Хобби")
@@ -12,7 +14,7 @@ class SearchRepository {
         val category = categories[i % categories.size]
         Ad(
             authorName = "Имя $i",
-            authorGroup = "$category",
+            authorGroup = category,
             authorAvatar = R.drawable.avatar_placeholder,
             image = R.drawable.ad_image_placeholder,
             title = "$category - Объявление $i",
@@ -32,21 +34,28 @@ class SearchRepository {
         )
     }
 
-    fun getInitialResults(): Pair<List<Ad>, List<Group>> {
-        return allAds.take(5) to allGroups.take(5)
+    //Для имитации нагрузки используем delay(1000)
+    fun getInitialResults(): Flow<Pair<List<Ad>, List<Group>>> = flow {
+        delay(1000)
+        emit(allAds.take(5) to allGroups.take(5))
     }
 
-    fun search(query: String): Pair<List<Ad>, List<Group>> {
+    fun search(query: String): Flow<Pair<List<Ad>, List<Group>>> = flow {
+        delay(1000)
         val filteredAds = allAds.filter { it.title.contains(query, ignoreCase = true) }
         val filteredGroups = allGroups.filter { it.name.contains(query, ignoreCase = true) }
-        return filteredAds to filteredGroups
+        emit(filteredAds.take(5) to filteredGroups.take(5))
     }
 
-    fun loadMoreAds(currentSize: Int): List<Ad> {
-        return allAds.drop(currentSize).take(10)
+    fun loadMoreAds(query: String, offset: Int): Flow<List<Ad>> = flow {
+        delay(1000)
+        val filteredAds = allAds.filter { it.title.contains(query, ignoreCase = true) }
+        emit(filteredAds.drop(offset).take(10))
     }
 
-    fun loadMoreGroups(currentSize: Int): List<Group> {
-        return allGroups.drop(currentSize).take(10)
+    fun loadMoreGroups(query: String, offset: Int): Flow<List<Group>> = flow {
+        delay(1000)
+        val filteredGroups = allGroups.filter { it.name.contains(query, ignoreCase = true) }
+        emit(filteredGroups.drop(offset).take(10))
     }
 }
